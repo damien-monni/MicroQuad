d89ff3b48aaec94577443c1a1f56479ad19dac5c
Damien Monni <monni.damien@gmail.com>
2014-02-10 23:14:42 +0100

    Try single PMW but doesnt work

--- 01 - Test moteur/Calibration ESC/main.c
+++ 01 - Test moteur/Calibration ESC/main.c
@@ -18,10 +18,11 @@ const float clockSourceMhz = 1.0f;
 const uint8_t prescaler = 1;
 
 //Temps de l'impulsion PMW pour un signal de puissance maximale
-const unsigned uint16_t maxSpeedMs = 700;
+volatile uint32_t maxSpeedMs = 2000;
 
 //Donne l'état du signal PMW (bas ou haut)
-volatile boolean isHigh;
+volatile uint8_t isHigh = 0;
+volatile int count = 0;
 
 
 int main(void){
@@ -30,13 +31,19 @@ int main(void){
 	TIMSK1 |= (1<<OCIE1A); //Interruption déclenchée lorsque OCR1A est atteint
 	OCR1A = usToTicks(maxSpeedMs); //Configure la première interruption à la fin de la première impulsion.
 	
-	DDRB |= 1<<DDB0; //Broche PB0 configurer comme sortie
-	PORTB = 1<<PORTB0; //Broche PB0 à l'état haut
-	isHigh = true; //Signal PMW à l'état haut
+	DDRD |= 1<<DDD4; //Broche PB0 configurer comme sortie
+	PORTD = 1<<PORTD4; //Broche PB0 à l'état haut
+	isHigh = 1; //Signal PMW à l'état haut
 	
 	sei(); //Activer les interruptions
 	
-	while(1); //Boucle infinie
+	while(1){ //Boucle infinie
+		if(count > 200){
+			maxSpeedMs = 700;
+			DDRB |= 1<<DDB6;
+			PORTB |= 1<<PORTB6;
+		}
+	}
 
 	return 0;
 }
@@ -45,16 +52,18 @@ int main(void){
 ISR(TIMER1_COMPA_vect)
 {
 	//Signal PMW à l'état haut.
-	if(isHigh){
-		PORTL &= ~(1<<PORTL0); //Passer le signal à l'état bas, terminer l'impulsion
+	if(isHigh == 1){
+		PORTD &= ~(1<<PORTD4); //Passer le signal à l'état bas, terminer l'impulsion
 		OCR1A = usToTicks(20000); //Configurer la prochaine interruption afin de créer un signal à 50Hz
-		isHigh = false;
+		isHigh = 0;
+		count++;
 	}
 	//Signal PMW à l'état bas, fin de la période de 20ms (50Hz)
 	else{
 		TCNT1 = 0; //Remettre timer à 0
-		PORTL |= 1<<0; //Commencer une impulsion
+		PORTD |= 1<<PORTD4; //Commencer une impulsion
 		OCR1A = usToTicks(maxSpeedMs); //Configurer la prochaine interruption pour la fin de l'impulsion
+		isHigh = 1;
 	}
 }
 
