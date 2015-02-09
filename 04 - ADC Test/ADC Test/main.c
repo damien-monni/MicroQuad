08e2e1e73e70af7a15b9e7e19c700bc9c4f10dfc
Damien Monni <monni.damien@gmail.com>
2015-02-09 21:43:09 +0100

    backup ADC code and start a test

--- 04 - ADC Test/ADC Test/main.c
+++ 04 - ADC Test/ADC Test/main.c
@@ -1,169 +1,36 @@
-/**************************************
-Damien MONNI - 28/02/2014 (last update : 28/02/2014)
-www.damien-monni.fr
-
-Fait tourner un moteur en fonction de l'inclinaison du drone. 
-**************************************/
-
-#include <avr/io.h> 
+#include <avr/io.h>
 #include <avr/interrupt.h>
 
-//*********************************************//
-//********* Prototypes de fonctions ***********//
-//*********************************************//
-
-//Initialise l'ADC et démarre une première conversion
-void initAdc();
-
-//Initialise le PMW permettant le contrôle des ESC pour moteurs brushless
-void initPmw();
-
-//Converti x d'une plage à une autre
-long map(long x, long in_min, long in_max, long out_min, long out_max);
-
-
-//*********************************************//
-//********* Variables globales ***********//
-//*********************************************//
-
-//PMW variables
-volatile unsigned int servo[4] = {700, 700, 700, 700};
-volatile int8_t channel = 1; //Controlled motor number : 1, 2, 3 or 4
-
-//ADC variables
-volatile uint16_t adc = 0;
-
-//Compte le nombre de seconde/50 écoulées depuis le lancement du programme
-//S'incrémente à 50Hz via la construction du signal PMW
-volatile uint16_t timeS = 0;
-
-
-//*********************************************//
-//********* Programme principal ***********//
-//*********************************************//
-int main(void){
-	
-	initPmw();
-	initAdc();
-	
-	sei(); //Activer les interruptions
-	
-	//Démarrer une première conversion des ADC
-	ADCSRA |= 1<<ADSC;
-	
-	while(1){
-	
-		//Après initialisation des ESC (après 7s)
-		if(timeS > 7*50){
-			
-			if(adc < 512){
-				adc = 512;
-			}
-			else if(adc > 1023){
-				adc = 1023;
-			}
-			
-			servo[0] = map(adc, 512, 1023, 700, 1000);
-		}
-		
-	}
-	
-	return 0;
-
-}
-
-//*********************************************//
-//********* Interruptions ***********//
-//*********************************************//
-
-//Construction du signal PMW
-ISR(TIMER1_COMPA_vect)
+int main(void)
 {
-	if(channel < 0){ //Tous les ESC ont reçus leurs impulsions, on attends la prochaine période
-		if(TCNT1 >= 20000){ //Fin de la période à 5Hz
-			TCNT1 = 0;
-			channel = 1;
-			PORTD |= 1<<channel;
-			OCR1A = servo[0];
-			timeS++;
-		}
-		else{
-			OCR1A = 20000; //Attendre la fin de la période
-		}
-	}
-	else{
-		if(channel < 4){ //Création de l'impulsion sur l'ESC suivant
-			OCR1A = TCNT1 + servo[channel];
-			PORTD &= ~(1<<channel); //Fin de l'impulsion sur l'ESC en cours
-			PORTD |= 1<<(channel + 1); //Début de l'impulsion sur l'ESC suivant
-			channel++;
-		}
-		else{
-			PORTD &= ~(1<<channel); //Fin de l'impulsion sur l'ESC en cours
-			OCR1A = TCNT1 + 500; //Rappeler l'interruption quelques microsecondes après
-			channel = -1; //Attendre la prochaine période
-		}
-	}
-}
-
-//Interruption lorsqu'une conversion de l'ADC se termine
-//Conversion en boucle
-ISR(ADC_vect){
-
-	//Lire le resultat
-	adc = ADC;
-	
-	//Sélectionner le convertisseur suivant
-	/*if(ADMUX == 5){
-		ADMUX = 0;
-	}
-	else{
-		ADMUX++;
-	}*/
-	
-	//Démarrer une nouvelle conversion
-	ADCSRA |= 1<<ADSC;
-	
-}
-
-//*********************************************//
-//********* Fonctions ***********//
-//*********************************************//
-
-//Initialise le PMW permettant le contrôle des ESC pour moteurs brushless
-void initPmw(){
-
-	TCCR1B |= 1<<CS11; //Prescaler 8 avec une horlge à 8Mhz
-	TIMSK1 |= (1<<OCIE1A); //Interruption sur OCR1A
-	OCR1A = servo[0]; //Prévoir la première interuption sur servo[0]
-	
-	DDRD |= 1<<DDD1 | 1<<DDD2 | 1<<DDD3 | 1<<DDD4; //Sortie vers les ESC
-	PORTD = 1<<channel; //Première impulsion PMW
-	
-}
-
-//Initialise l'ADC et démarre une première conversion
-void initAdc(){
-
-	//Activer l'ADC via ADEN dans ADCSRA
-	ADCSRA |= 1<ADEN;
-
-	//Selectionner la référence de voltage via REFSn dans ADMUX
-		//Rien à faire car utilisation de AREF, donc REFS0 et RES1 = 0
-	
-	//Sélectionner le convertisseur ADC5 via MUX dans ADMUX
+	ADCSRA |= 1<<ADPS2 | 1<<ADPS1;
+	ADMUX |= 1<<REFS0;
 	ADMUX |= 1<<MUX0 | 1<<MUX2;
-	
-	//Activer l'interruption sur conversion terminée via ADIE dans ADCSRA
 	ADCSRA |= 1<<ADIE;
-	
-	//Paramétrer le prescaler de l'ADC à 64 :horloge 8Mhz -> horloge ADC de 125kHz
-	ADCSRA |= 1<<ADPS1 | 1<<ADPS2;
-	
+	ADCSRA |= 1<<ADEN;
+
+	sei();
+
+	ADCSRA |= 1<<ADSC;
+
+	while (1)
+	{
+	}
 }
 
-//Converti x d'une plage à une autre
-long map(long x, long in_min, long in_max, long out_min, long out_max)
+ISR(ADC_vect)
 {
-	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+	uint8_t theLowADC = ADCL;
+	uint16_t theTenBitResults = ADCH<<8 | theLowADC;
+	
+	if(theTenBitResults == 0){
+		DDRB |= 1<<DDB0;
+		PORTB |= 1<<PORTB0;
+	}
+	else{
+		DDRB |= 1<<DDB0;
+		PORTB &= ~(1<<PORTB0);		
+	}
+
+	ADCSRA |= 1<<ADSC; 
 }
