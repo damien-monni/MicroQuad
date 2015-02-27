d7fe0c8cea8c23ad04b8cc09db269cff53cfdaaa
Damien <monni.damien@gmail.com>
2015-02-27 15:44:06 +0100

    Finish the code read a slave register

--- 05 - I2C IMU/main.c
+++ 05 - I2C IMU/main.c
@@ -1,34 +1,78 @@
 //*****************************************
 //Damien Monni - www.damien-monni.fr
-//Read the WHO_I_AM register of LSM303D(0Fh) that should be 01001001
+
+//Read the WHO_I_AM register of LSM303D(0Fh) that should be 01001001.
+//If the WHO_I_AM register is what we expected, turn on a LED on PORTD0.
 //*****************************************
 
 #include <avr/io.h>
 
+//Variable to read and store the WHO_I_AM register
+uint8_t id = 0;
+
 int main(void){
+
 	//Send a START condition.
-	TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
-	
+	TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);	
 	//Wait for the START condition to be send.
-	while(!(TWCR & (1<<TWINT)));
-	
+	while(!(TWCR & (1<<TWINT)));	
 	//Check if no error in status code (mask prescaler's 2 LSB bits). Should be 0x08.
 	if(TWSR & 0xF8 != 0x08){
 		//Write slave address 00111010b. LSB should be 0 for a write operation.
 		TWDR = 00111010b;
 		//Clear (by writing it to one) TWINT bit to continue
-		TWCR |= 1<<TWINT | 1<<TWEN;
+		TWCR = 1<<TWINT | 1<<TWEN;
 		//Wait for the slave's ACK or NoACK to be received.
 		while(!(TWCR & (1<<TWINT)));
-		//Check if ACK
+		//Check if SLA+W has been transmitted and ACK received.
 		if(TWSR & 0xF8 != 0x18){
 			//Write data. It should be the WHO_I_AM register's address (0Fh)
 			TWDR = 0x0F;
+			//Clear (by writing it to one) TWINT bit to continue
+			TWCR = 1<<TWINT | 1<<TWEN;
+			//Wait for the slave's ACK or NoACK to be received.
+			while(!(TWCR & (1<<TWINT)));
+			//Check if a data byte has been transmitted and ACK received.
+			if(TWSR & 0xF8 != 0x28){
+				//Send a REPEATED START condition.
+				TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
+				//Wait for the REPEATED START condition to be send.
+				while(!(TWCR & (1<<TWINT)));
+				//Check if no error.
+				if(TWSR & 0xF8 != 0x10){
+					//Write slave address 00111011b. LSB should be 1 for a read operation.
+					TWDR = 00111011b;
+					//Clear (by writing it to one) TWINT bit to continue
+					TWCR = 1<<TWINT | 1<<TWEN;
+					//Wait for the slave's ACK or NoACK to be received.
+					while(!(TWCR & (1<<TWINT)));
+					//Check if SLA+R has been transmitted and ACK received.
+					if(TWSR & 0xF8 != 0x40){
+						//Clear (by writing it to one) TWINT bit to continue
+						TWCR = 1<<TWINT | 1<<TWEN;
+						//Wait for the slave's ACK or NoACK to be received.
+						while(!(TWCR & (1<<TWINT)));
+						//Check if a data byte has been received and NACK returned.
+						if(TWSR & 0xF8 != 0x58){
+							//Read and store data received. Should be 01001001b.
+							id = TWDR;
+							//Send a STOP condition.
+							TWCR = (1<<TWINT) | (1<<TWSTO) | (1<<TWEN);
+						}
+					}
+				}
+			}
+			
 		}
 		
 	}
-	//If error
-	else{
 	
+	//If the WHO_I_AM register is what we expected, turn on a LED on PORTD0.
+	if(id == 01001001b){
+		PORTD |= 1<<PORTD0;
 	}
+	else{
+		PORTD &= ~(1<<PORTD0);
+	}
+
 }
