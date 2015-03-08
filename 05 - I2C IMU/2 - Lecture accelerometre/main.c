5ddaddd68acb2d9de56f498825f4a925d724b904
Damien Monni <monni.damien@gmail.com>
2015-03-08 12:20:09 +0100

    Remove useless comments

--- 05 - I2C IMU/2 - Lecture accelerometre/main.c
+++ 05 - I2C IMU/2 - Lecture accelerometre/main.c
@@ -4,7 +4,6 @@
 //Read multiple registers from LSM303D.
 //Read accelerator values (2 bytes per value) on X, Y and Z.
 //Read registers are 0x28 - 0x29 / 0x2A - 0x2B / 0x2C - 0x2D 
-//Read it without blocking code.
 //*****************************************
 
 #include <avr/io.h>
@@ -26,119 +25,6 @@ typedef struct{
 
 Axis accelerometer = {0};
 
-
-//**********************************//
-//TWI Functions
-//**********************************//
-
-/*
-void twiWaitFlag(){
-	while(!(TWCR & (1<<TWINT)));
-}
-
-//Get status code
-uint8_t twiGetStatus(){
-	return (TWSR & 0xF8);
-}
-
-//Write a byte
-void twiWriteByte(uint8_t byte){
-	TWDR = byte;
-	//Clear (by writing it to one) TWINT bit to continue
-	TWCR = 1<<TWINT | 1<<TWEN;
-}
-
-//Initialize a TWI communication sending a Start and SLA+R/W
-uint8_t twiInit(uint8_t slaveAddress, uint8_t isRead){
-	//Send a (RE)START
-	TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
-	while(!(TWCR & (1<<TWINT)));
-	if((twiGetStatus() == 0x08) || (twiGetStatus() == 0x10)){
-		TWDR = ((slaveAddress << 1) | isRead);
-		TWCR = 1<<TWINT | 1<<TWEN;
-	}
-	while(!(TWCR & (1<<TWINT)));
-	
-	return (TWSR & 0xF8);
-}
-
-//Write only one byte 
-uint8_t twiWriteOneByte(uint8_t slaveAddress, uint8_t slaveRegister, uint8_t data){
-	
-	if(twiInit(slaveAddress, 0) == 0x18){
-		twiWriteByte(slaveRegister);
-		twiWaitFlag();
-		if(twiGetStatus() == 0x28){
-			twiWriteByte(data);
-			twiWaitFlag();
-			if(twiGetStatus() == 0x28){
-				return 1;
-			}
-		}
-	}
-
-	return 0;
-}
-
-//Read only one byte
-uint8_t twiReadOneByte(uint8_t slaveAddress, uint8_t slaveRegister){
-
-	uint8_t value = 0;
-	
-	if(twiInit(slaveAddress, 0) == 0x18){
-		twiWriteByte(slaveRegister);
-		twiWaitFlag();
-		if(twiGetStatus() == 0x28){
-			if(twiInit(slaveAddress, 1) == 0x40){
-				TWCR = 1<<TWINT | 1<<TWEN;
-				while(!(TWCR & (1<<TWINT)));
-				if((TWSR & 0xF8) == 0x58){
-					value = TWDR;
-					TWCR = 1<<TWINT | 1<<TWEN;
-				}
-			}
-		}
-	}
-	
-	return value;
-	
-}
-
-//Read multiple bytes
-//Return 1 if OK, 0 if error.
-uint8_t twiReadMultipleBytes(uint8_t slaveAddress, uint8_t slaveRegister, uint8_t result[], uint8_t nbBytes){
-	
-	uint8_t i;
-	
-	if(twiInit(slaveAddress, 0) == 0x18){
-		twiWriteByte((slaveRegister | (1<<7)));
-		twiWaitFlag();
-		if(twiGetStatus() == 0x28){
-			if(twiInit(slaveAddress, 1) == 0x40){
-				for(i = 0 ; i < nbBytes ; i++){
-					TWCR = 1<<TWINT | 1<<TWEN | 1<<TWEA;
-					while(!(TWCR & (1<<TWINT)));
-					if((TWSR & 0xF8) == 0x50){
-						result[i] = TWDR;
-					}
-					else{
-						break;
-					}
-				}
-				if(i == nbBytes){
-					return 1;
-				}
-			}
-		}
-	}
-	
-	return 0;
-	
-}
-
-*/
-
-
 //**********************************//
 //Main
 //**********************************//
@@ -158,15 +44,13 @@ int main(void){
 	TWBR = 0x02;
 	
 	
-	//**********************************//
 	//Enable X, Y, Z accelerometer's axis and
-	//set data rate selection to 400Hz.
-	//**********************************//
-	
+	//set data rate selection to 400Hz.	
 	while(twiWriteOneByte(0b0011101, 0x20, 0b10000111) == 0);
 	
-	uint8_t result[6];
 	
+	//Get results
+	uint8_t result[6];
 	while(1){
 	
 		if(twiReadMultipleBytes(0b0011101, 0x28, result, 6) == 1){
