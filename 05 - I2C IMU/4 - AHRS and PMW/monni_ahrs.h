6e0cfc2b666450b1af5d99c7956240ac339bfa03
Damien <monni.damien@gmail.com>
2015-03-18 15:11:21 +0100

    Create a monni_ahrs.h file

---
+++ 05 - I2C IMU/4 - AHRS and PMW/monni_ahrs.h
@@ -0,0 +1,11 @@
+//*****************************************
+//Damien Monni - www.damien-monni.fr
+//
+//*****************************************
+
+#ifndef MONNI_AHRS
+#define MONNI_AHRS
+
+
+
+#endif
